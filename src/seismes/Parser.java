b024c4457951186c382990995a9b0b87f15ffb5f
Eric Thivierge <thiveri@gmail.com>
2010-07-07 13:55:10 -0400

    pull de vince, 7juillet.

---
+++ src/seismes/Parser.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package seismes;
+
+/**
+ * @author Eric Thivierge
+ *
+ */
+public class Parser {
+	private final char
+		SEP = ',';
+	
+	public Parser() {
+		
+	}
+	
+	public Entree parseLine(String line) {
+		
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
